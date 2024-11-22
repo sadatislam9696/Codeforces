4216dedc81aa8baed5d008da68129efd408aa1b5
sadat islam abdullah <122301741+sadatislam9696@users.noreply.github.com>
2024-11-22 23:14:12 +0600

    Create 2028B - Alice's Adventures in Permuting.cpp

---
+++ 2028B - Alice's Adventures in Permuting.cpp
@@ -0,0 +1,59 @@
+/***   Bismillahir Rahmanir Rahim   ***/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define     nn          '\n'
+#define     ss          second
+#define     ff          first
+#define     sp(d, n)    fixed << setprecision(d) << n
+#define     srt(v)      sort(v.begin(),v.end());
+#define     rsrt(v)     sort(v.rbegin(),v.rend());
+#define     rev(v)      reverse(v.begin(),v.end());
+#define     mem(a, x)   memset(a, x, sizeof a)
+
+using ll =  long long;
+using ld =  long double;
+using ull = unsigned long long;
+const ld pi = acos(-1.0);
+template <typename T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+int dx[6] = {0, 1, -1, 0, 0};
+int dy[6] = {0, 0, 0, 1, -1};
+
+const int M = 1e9 + 7;
+const int N = 1e3 + 9;
+
+void solution(){
+   ll n, b, c; 
+   cin >> n >> b >> c; 
+
+   if(b == 0){
+     if(c >= n){
+        cout << n << nn;
+     }
+     else if(c >= (n - 2)){
+        cout << n - 1 << nn;
+     }
+     else cout << -1 << nn;
+   }
+   else{
+     ll x = max<ll>(0, ceil((n - c) / ((ld)b)));
+     cout << n - x << nn;
+   }
+}
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+
+    int t = 1, c = 1;
+    cin >> t;
+    //cin.ignore();
+    while(t--){
+        //cout << "Case " << c++ << ": ";
+        solution();
+    }
+    return 0;
+}
